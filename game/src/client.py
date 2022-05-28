71bd50d912429a1c1a99623fdad4120bc8cbcb05
hy <1025564026@qq.com>
2022-05-28 16:59:40 +0800

    finish client

--- game/src/client.py
+++ game/src/client.py
@@ -6,8 +6,10 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
+from sys import stdin
 
-def main():
+
+def operate(op, user_id, username, score):
     # Make socket
     transport = TSocket.TSocket('localhost', 9090)
 
@@ -23,11 +25,23 @@ def main():
     # Connect!
     transport.open()
 
-    user = User(1, 'yxc', 1500)
-    client.add_user(user, "")
+    user = User(user_id, username, score) # 添加用户
+
+    # 如果选项为增加用户则执行增加用户函数，反之执行删除用户函数
+    if op == "add":
+        client.add_user(user, "")
+    elif op == "remove":
+        client.remove_user(user, "")
+
 
     # Close!
     transport.close()
+
+def main():
+    # 从终端读入信息
+    for line in stdin:
+        op, user_id, username, score = line.split(' ')
+        operate(op, int(user_id), username, int(score))
 
 if __name__ == "__main__":
     main()
